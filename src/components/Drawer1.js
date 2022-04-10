8645a463c09c5547587176224c2b933fe08b8cb4
Junku Kim <kimjunku0320@gmail.com>
2022-04-10 23:30:09 +0900

    2022/04/10 - Change Avatar Image

--- src/components/Drawer1.js
+++ src/components/Drawer1.js
@@ -114,7 +114,7 @@ export default function PersistentDrawerLeft() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              <Avatar src="/broken-image.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
